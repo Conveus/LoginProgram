3333ef3f8634f8c04530217a669614ee672e2c7e
Connor Hutnell <connorhutnell@yahoo.co.uk>
2023-04-20 16:43:14 +0100

    Modified LoginController methods to use Files instead of variables for permanent accounts Added but not implemented a method to delete accounts

--- src/LoginController.java
+++ src/LoginController.java
@@ -1,29 +1,145 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileWriter;
 import java.util.Scanner;
 
 public class LoginController {
 
-    Scanner input = new Scanner(System.in);
+    // Location to save account files to
+    public String location = "C:\\Users\\conno\\IdeaProjects\\LoginProgram\\src\\Accounts\\";
+
     // Create temp account details
     private String username = "user";
     private String password = "1234";
 
     // Register account
-    // TODO: Write accounts to file
-    public void registerAccount(String user, String pass){
-        username = user.trim();
-        password = pass;
+    // DONE: Write accounts to file
+    public boolean registerAccount(String user, String pass) throws IOException{
+        //username = user.trim();
+        //password = pass;
+        // Creates variable for filename from user input
+        String fileName = location+user.toLowerCase().trim()+".txt";
+        File account = new File(fileName);
+        //Checks account doesn't already exist
+        if(!account.exists()){
+            // Creates account
+            if(account.createNewFile()){
+                System.out.println("Account created");
+                recordAccountDetails(user, pass, fileName);
+                setUsername(user);
+                setPassword(pass);
+                return true;
+            }else{
+                System.out.println("Account could not be created");
+                return false;
+            }
+        }
+        else{
+            System.out.println("Account with that name already exists");
+            return false;
+        }
     }
 
-    // Check account details
-    // TODO: Check account(s) in file
-    public boolean checkAccount(String user, String pass)
+    // Writes username and password into account file
+    public boolean recordAccountDetails(String user, String pass, String file){
+        try{
+            FileWriter myWriter = new FileWriter(file);
+            // Writes username on first line and password on second
+            myWriter.write(user + pass);
+            myWriter.close();
+            System.out.println("Successfully wrote to file");
+            return true;
+        } catch (IOException e) {
+            System.out.println("An error occurred");
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    // Check account details are correct
+    // DONE: Check account(s) in file
+    public boolean checkAccount(String user, String pass) throws IOException
     {
-        if((username.toLowerCase()).equals(user.toLowerCase().trim()) && password.equals(pass.trim())){
+        /*if((username.toLowerCase()).equals(user.toLowerCase().trim()) && password.equals(pass.trim())){
             return true;
         }
         else{
             return false;
+        }*/
+
+        // Creates variable for filename from user input
+        String fileName = location+user.toLowerCase().trim()+".txt";
+        File account = new File(fileName);
+        try{
+            Scanner reader = new Scanner(account);
+            String data = "";
+            // Checks account exists
+            if(account.exists()){
+                while (reader.hasNextLine())
+                {
+                    data = reader.nextLine();
+                }
+                reader.close();
+                if(data.equals(user+pass)){
+                    setUsername(user);
+                    setPassword(pass);
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }else {
+                return false;
+            }
+        }catch (FileNotFoundException e){
+            System.out.println("File not found");
+            e.printStackTrace();
+            return false;
         }
+    }
+
+    public boolean deleteAccount(){
+        String fileName = location+username.toLowerCase().trim()+".txt";
+        File account = new File(fileName);
+        // Checks account exists
+        if(account.exists())
+        {
+            Scanner input = new Scanner(System.in);
+            // Check the user want the file deleted
+            System.out.println("Are you sure you want to delete the account " + username + "?");
+            System.out.print("Please type the account name to confirm: ");
+            String confirm = input.nextLine().trim();
+            if(confirm.equals(username))
+            {
+                // Deletes file
+                if(account.delete())
+                {
+                    System.out.println("Deleted the account: " + username);
+                    return true;
+                }
+                else {
+                    System.out.println("Failed to delete account");
+                    return false;
+                }
+            }
+            else {
+                System.out.println("Incorrect entry cancelling account deletion");
+                return false;
+            }
+        }
+        else {
+            System.out.println("Account does not exist");
+            return false;
+        }
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getUsername() {
--- src/Tests/Tests.java
+++ src/Tests/Tests.java
@@ -1,8 +1,10 @@
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Tests {
 
-    @org.junit.jupiter.api.Test
+    /*@org.junit.jupiter.api.Test
     void AccountCheckShouldReturnFalse(){
         var loginControl = new LoginController();
         assertFalse(loginControl.checkAccount("wrong","wrong"));
@@ -24,5 +26,47 @@ public class Tests {
     void AccountCheckShouldReturnTrueEvenWithSpacesAtTheEndOfAUsername(){
         var loginControl = new LoginController();
         assertTrue(loginControl.checkAccount("user  ","1234"));
+    }*/
+
+    @org.junit.jupiter.api.Test
+    void RegisterShouldCreateFileInAccountFolderUsingUsername() throws IOException {
+        var loginControl = new LoginController();
+        assertTrue(loginControl.registerAccount("Test","Test"));
     }
+
+    @org.junit.jupiter.api.Test
+    void RegisterShouldNotCreateFileIfAlreadyExists() throws IOException {
+        var loginControl = new LoginController();
+        assertFalse(loginControl.registerAccount("Test","Test"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void RecordShouldWriteAccountDetailsToFile() throws IOException {
+        var loginControl = new LoginController();
+        assertTrue(loginControl.recordAccountDetails("Test","Test", loginControl.location+"test.txt"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void CheckAccountShouldReturnTrueOnCorrectDetails() throws IOException {
+        var loginControl = new LoginController();
+        assertTrue(loginControl.checkAccount("Test", "Test"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void CheckAccountShouldReturnFalseOnWrongDetails() throws IOException {
+        var loginControl = new LoginController();
+        assertFalse(loginControl.checkAccount("Test", "test"));
+    }
+
+    /*@org.junit.jupiter.api.Test
+    void DeleteAccountShouldNotDeleteTheFileIfConfirmationNotGiven() throws IOException {
+        var loginControl = new LoginController();
+        assertTrue(loginControl.deleteAccount());
+    }
+
+    @org.junit.jupiter.api.Test
+    void DeleteAccountShouldDeleteTheFileIfConfirmationGiven() throws IOException {
+        var loginControl = new LoginController();
+        assertTrue(loginControl.deleteAccount());
+    }*/
 }
